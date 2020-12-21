d1e69d5809f2d1ef2fecaf33c1e6f523e78ecbc6
bkahloon <kahlonbakht@gmail.com>
2020-12-20 22:48:45 -0700

    Set up module

---
+++ src/main/scala/ExampleUsage/scala.scala
@@ -0,0 +1,8 @@
+package ExampleUsage
+
+import org.apache.flink.streaming.api.scala._
+
+
+class scala {
+
+}
--- src/main/scala/serializer/AvroStateSerializer.scala
+++ src/main/scala/serializer/AvroStateSerializer.scala
@@ -1,5 +1,17 @@
 package serializer
+import com.sksamuel.avro4s.{Decoder, Encoder, SchemaFor}
+import org.apache.flink.api.common.typeutils.TypeSerializer
+import org.apache.flink.core.memory.{DataInputView, DataOutputView}
+import scala.reflect.ClassTag
 
-class AvroStateSerializer {
+class AvroStateSerializer[T<: Product: Encoder: Decoder: SchemaFor: ClassTag[T] ] extends TypeSerializer[T] {
+
+  override def serialize(record: T, target: DataOutputView): Unit = {
+
+  }
+
+  override def deserialize(source: DataInputView): T = {
+
+  }
 
 }
